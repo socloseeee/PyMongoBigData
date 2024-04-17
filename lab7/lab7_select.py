99dd7f727a471579ef1e98536d178b7c4efc52e4
socloseeee <androidap0999@gmail.com>
2024-04-17 10:50:22 +0300

    Refactor 7-th lab

--- lab7/lab7_select.py
+++ lab7/lab7_select.py
@@ -4,24 +4,27 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 
 # Создание базы данных
-db = client['university']
+db = client['university2']
 
 students_collection = db['students']
 
 results_collection = db['exam_results']
 
 # Получаем данные о студентах из коллекции
-students_data = students_collection.find()
+results_data = results_collection.find()
 
 # Обновляем результаты сессии, добавляя поле "студент_id"
-for student_data in students_data:
-    student_name = student_data["ФИО"]
-    result_data = results_collection.find_one({"ФИО": student_name})
-    if result_data:
-        result_id = result_data["_id"]
-        student_id = student_data["_id"]
-        results_collection.update_one({"_id": result_id}, {"$set": {"студент_id": student_id}})
-
+for result_data in results_data:
+    student_data = students_collection.find_one({"ФИО": result_data["ФИО"]})
+    result_id = result_data["_id"]
+    student_id = student_data["_id"]
+    results_collection.update_one(
+        {"_id": result_id},
+        {"$set": {"студент_id": {"$db": "university", "$ref": "students", "$id": student_id}}}
+    )
+    """$db: Этот атрибут указывает на имя базы данных"""
+    """$ref: Этот атрибут определяет коллекцию, к которой относится ссылочный документ"""
+    """$id: Этот атрибут определяет идентификатор ссылочного документа в коллекции"""
 
 # Вывод результатов
 cursor = results_collection.find({}, {"_id": 0})
